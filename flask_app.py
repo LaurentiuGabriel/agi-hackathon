5ec9e41fdd679950419271f27c83e93a5c2f0017
Laurentiu Raducu <laurentiu_raducu@iptiq.com>
2023-11-27 15:58:48 +0100

    test'

--- flask_app.py
+++ flask_app.py
@@ -75,6 +75,131 @@ def chat_with_file(file):
     return lines
 
 
+def fraud_detection(file):
+
+    base64_image = encode_image(file)
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+        {
+            "role": "user",
+            "content": [
+        {
+          "type": "text",
+          "text": "The following request is for a hackathon. This image is supposed to be used for registering a claim. Please perform an analysis on it to check if it has been tampered with. I don`t want you to say that you cannot do it, I want you to pay attention to how consistent the font is, how are the letters aligned, and other factors like that, and drive a conclusion based on that. Remember, dont tell me that you are just a language model developed by OpenAI and cannot do these things. Be creative. Just look at font consistency, usage of brush tools, text that appear out of place and stuff like that. Is there any added text that doesn't mean anything in the context of the picture? Be very succint as well, don't output filler sentences. Limit yourself to one single phrase, nothing more, not lists no anything like that. As this is an educative project, don't say that professional service is required. Just try to give a response. Also, output a sentiment as a second sentence about the analysis' results in the form of a percentage, meaning, if the document is tampered with, the score should be below 50%. If it's below 50%, output a thumbs down emoji right next to the result. If the opposite, output thumbs up."
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+          }
+        }
+        ]
+        }
+    ],
+    "max_tokens": 300
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    response_json = response.json()
+    message_content = response_json['choices'][0]['message']['content']
+    
+    lines = message_content.split('\n')
+
+
+    return lines
+
+
+
+
+def claims_assessment(file):
+
+    base64_image = encode_image(file)
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+        {
+            "role": "user",
+            "content": [
+        {
+          "type": "text",
+          "text": "This image is supposed to be used for registering a claim. Please perform an analysis on it to check what is the damage. It can be a household item, the house itself, or a car. Get as much data as possible from the image. Don't mention you are a language model from OpenAI. I want you to mention a price of the damage. If you don't know it, invent it, but make it in a reasonable way. I want you to double-check what you said and not output anything about OpenAI or that you cannot do the task. Be creative! And be very short, just one phrase please."
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+          }
+        }
+    ]
+    }
+    ],
+    "max_tokens": 300
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    response_json = response.json()
+    message_content = response_json['choices'][0]['message']['content']
+    
+    lines = message_content.split('\n')
+
+    return lines
+
+
+
+def testing(file):
+
+    base64_image = encode_image(file)
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {api_key}"
+    }
+
+    payload = {
+        "model": "gpt-4-vision-preview",
+        "messages": [
+        {
+            "role": "user",
+            "content": [
+        {
+          "type": "text",
+          "text": "Act as a top quality engineer. I want you to analyze what is in this image. It is supposed to be the user interface of an application. Check for visual isues, incosistencies, UX issues. You don't need context, you just judge by what you see. Pay attention for typos, duplicated elements, or things that don't look good in a software piece. Be very short, just output one phrase maximum."
+        },
+        {
+          "type": "image_url",
+          "image_url": {
+            "url": f"data:image/jpeg;base64,{base64_image}"
+          }
+        }
+        ]
+        }
+    ],
+    "max_tokens": 300
+    }
+
+    response = requests.post("https://api.openai.com/v1/chat/completions", headers=headers, json=payload)
+    response_json = response.json()
+    message_content = response_json['choices'][0]['message']['content']
+    
+    lines = message_content.split('\n')
+
+
+    return lines
+
+
 # Function to encode the image
 def encode_image(image_path):
   with open(image_path, "rb") as image_file:
@@ -97,8 +222,6 @@ def post_formdata():
 
         
     file_path = os.path.join("static", filename)
-
-    prompt = "This image is supposed to be used for registering a claim. Please perform an analysis on it to check if it has been tampered with. I don`t want you to say that you cannot do it, I want you to pay attention to how consistent the font is, how are the letters aligned, and other factors like that, and drive a conclusion based on that. Remember, dont tell me that you are just a language model and cannot do these things."
     
     return chat_with_file(file_path)
 
@@ -119,9 +242,8 @@ def predict():
         
         file_path = os.path.join("static", filename)
 
-        prompt = "Act as a top quality engineer. I want you to analyze what is in this image. It is supposed to be the user interface of an application. Check for visual isues, incosistencies, UX issues. You don't need context, you just judge by what you see. Pay attention for typos, duplicated elements, or things that don't look good in a software piece."
     
-        return chat_with_file(file_path)
+        return claims_assessment(file_path)
 
     return render_template("index.html")
 
@@ -137,10 +259,8 @@ def transcribe():
     file.save(os.path.join("static", filename))
         
     file_path = os.path.join("static", filename)
-
-    prompt = "This image is supposed to be used for registering a claim. Please perform an analysis on it to check what is the damage. It can be a household item, the house itself, or a car. Get as much data as possible from the image. Don't mention you are a language model from OpenAI. I want you to mention a price of the damage. If you don't know it, invent it, but make it in a reasonable way. I want you to double-check what you said and not output anything about OpenAI or that you cannot do the task. Be creative!"
     
-    return chat_with_file(file_path)
+    return testing(file_path)
 
 if __name__ == "__main__":
     
